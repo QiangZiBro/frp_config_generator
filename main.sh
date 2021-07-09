8b2f1f4e115e7d990f0359b16f655c00a1c7fbdc
QiangZiBro <qiangzibro@gmail.com>
2021-07-09 15:23:21 +0800

    first commit

---
+++ main.sh
@@ -0,0 +1,81 @@
+USERNAME=qiangzibro
+SERVER_IPS=("10.22.148.86" "10.22.78.13")
+
+if [ -f "config" ]; then
+	rm config
+fi
+
+for j in {0..1}
+do
+
+	IP=${SERVER_IPS[j]}
+
+	for i in {0..5}
+	do
+
+		if [ $j = 0 ];then
+			HOST="l${i}"
+		elif [ $j = 1 ];then
+			HOST="l${i}_"
+		fi
+
+		frp_dir=frp_m${i}
+		frp_system=${frp_dir}/systemd/
+		mkdir -p $frp_dir $frp_system
+
+		cp setup.sh ${frp_dir}
+
+
+		cat <<- EOF >> config
+		Host $HOST
+		HostName $IP
+		Port 610${i}
+		User $USERNAME
+
+		EOF
+
+		cat <<- EOF > ${frp_dir}/frpc${j}.ini
+		[common]
+		server_addr = ${IP}
+		server_port = 7000
+		token = 1234567
+
+		[sshM${i}${j}]
+		type = tcp
+		local_ip = 127.0.0.1
+		local_port = 22
+		remote_port = 610${i}
+
+		[RDPM${i}${j}]
+		type = tcp
+		local_ip = 127.0.0.1
+		local_port = 3389
+		remote_port = 349${i}
+
+		[httpM${i}${j}]
+		type = tcp
+		local_ip = 127.0.0.1
+		local_port = 1100${i}
+		remote_port = 1100${i}
+
+		EOF
+
+		cat <<- EOF > ${frp_system}/frpc${j}.service
+		[Unit]
+		Description=Frp Client Service
+		After=network.target
+
+		[Service]
+		Type=simple
+		User=root
+		Restart=on-failure
+		RestartSec=5s
+		ExecStart=/usr/local/frp/frpc -c /usr/local/frp/frpc${j}.ini
+		ExecReload=/usr/local/frp/frpc reload -c /usr/local/frp/frpc${j}.ini
+
+		[Install]
+		WantedBy=multi-user.target
+		EOF
+
+	done
+done
---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -ex
+
+HOSTNAME=`hostname`
+USER=`whoami`
+frp_dir="/home/${USER}/frp_m${HOSTNAME: -1}"
+
+if [ -d "/usr/local/frp_m${HOSTNAME: -1}" ];then
+	sudo rm -rf "/usr/local/frp_m${HOSTNAME: -1}"
+fi
+sudo cp -r ${frp_dir} /usr/local
+
+cd /usr/local
+sudo rm -rf frp
+sudo ln -s "frp_m"${HOSTNAME: -1}  frp
+
+for i in {0..1}
+do
+	sudo cp /usr/local/frp/systemd/frpc${i}.service /lib/systemd/system/
+	sudo systemctl enable frpc${i}.service
+	sudo systemctl start frpc${i}.service
+	sudo systemctl restart frpc${i}.service
+
+done
